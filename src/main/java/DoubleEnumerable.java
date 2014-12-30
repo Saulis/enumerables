35361a46ea58aeb5bdf3b192d5f4077abcecc90a
Sauli Tähkäpää <sauli@vaadin.com>
2014-12-30 23:03:44 +0200

    Refactored min and max function variables.

--- src/main/java/DoubleEnumerable.java
+++ src/main/java/DoubleEnumerable.java
@@ -1,17 +1,12 @@
 import java.util.Optional;
-import java.util.function.BiFunction;
 
 public class DoubleEnumerable extends NumberEnumerable {
-    private static BiFunction<Double, Double, Double>
-            minFunction = (acc, x) -> Double.min(acc, x);
-    private static BiFunction<Double, Double, Double>
-            maxFunction = (acc, x) -> Double.max(acc, x);
 
     public static Optional<Double> min(Enumerable<Double> enumerable) {
-        return aggregate(enumerable, minFunction);
+        return aggregate(enumerable, (acc, x) -> Double.min(acc, x));
     }
 
     public static Optional<Double> max(Enumerable<Double> enumerable) {
-        return aggregate(enumerable, maxFunction);
+        return aggregate(enumerable, (acc, x) -> Double.max(acc, x));
     }
 }
--- src/main/java/IntEnumerable.java
+++ src/main/java/IntEnumerable.java
@@ -1,19 +1,13 @@
 import java.util.Optional;
-import java.util.function.BiFunction;
 
 public class IntEnumerable extends NumberEnumerable {
 
-    private static BiFunction<Integer, Integer, Integer>
-            minFunction = (acc, x) -> Integer.min(acc, x);
-    private static BiFunction<Integer, Integer, Integer>
-            maxFunction = (acc, x) -> Integer.max(acc, x);
-
     public static Optional<Integer> min(Enumerable<Integer> enumerable) {
-        return aggregate(enumerable, minFunction);
+        return aggregate(enumerable, (acc, x) -> Integer.min(acc, x));
     }
 
     public static Optional<Integer> max(Enumerable<Integer> enumerable) {
-        return aggregate(enumerable, maxFunction);
+        return aggregate(enumerable, (acc, x) -> Integer.max(acc, x));
     }
 
 
